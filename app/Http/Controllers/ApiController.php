732395d980437b797efafe7c9fb9b12857e0342b
Kristýna Tomanová <kristytoman@protonmail.com>
2021-04-01 13:14:18 +0200

    Controller refactoring

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -7,55 +7,102 @@ use App\Http\Resources\ProgramResource;
 use Illuminate\Http\Request;
 use Carbon\Carbon;
 
-
-
 class ApiController extends Controller
 {
-    public function getPrograms(int $type, string $id)
+    /**
+     * Fetch data from the third party API.
+     * 
+     * @param string  $path
+     * @return array|null
+     */
+    private function getContents(string $path)
     {
-        $res = file_get_contents(
-            "https://stag-ws.utb.cz/ws/services/rest2/programy/getStudijniProgramy?" .
-            "fakulta=" . $id . 
-            "&typ=" . $type .
-            "&outputFormat=JSON"
-        );
+        $res = file_get_contents($path);
         if ($res !== false && (strpos($res, "faultstring") !== false || substr($res, 0, 6) == '<html>')) {
-            return response()->json(['fail']);
+            return null;
         }
         if ($res === false || strlen($res) == 0) {
-            return response()->json(['fail']);
+            return null;
         }
-        $res = json_decode($res, true);
-        return json_encode(ProgramResource::collection($res['programInfo']), JSON_UNESCAPED_UNICODE);
+        return json_decode($res, true);
     }
 
+    /**
+     * Get array of university's study programs.
+     * 
+     * @param int  $type
+     * @param string  $id
+     * @return string|null
+     */
+    public function getPrograms(int $type, string $id)
+    {
+        if ($res = $this->getContents(
+                "https://stag-ws.utb.cz/ws/services/rest2/programy/getStudijniProgramy?" .
+                "fakulta=" . $id . 
+                "&typ=" . $type .
+                "&outputFormat=JSON"
+            )) {
+                return json_encode(
+                    ProgramResource::collection($res['programInfo']), 
+                    JSON_UNESCAPED_UNICODE
+                );
+        }
+        else return null;
+    }
+
+    /**
+     * Get array of program's fields.
+     * 
+     * @param int  $type
+     * @param string  $id
+     * @return Illuminate\Http\Response
+     */
     public function getFields(int $type, string $id)
     {
         $programs = json_decode($this->getPrograms($type, $id));
         $fields = [];
         foreach ($programs as $program) {
-            $res = file_get_contents(
-                'https://stag-ws.utb.cz/ws/services/rest2/programy/getOboryStudijnihoProgramu?' .
-                "stprIdno=" . $program->id .
-                "&rok=" . Carbon::now()->year .
-                "&outputFormat=JSON"
-            );
-            if ($res !== false && (strpos($res, "faultstring") !== false || substr($res, 0, 6) == '<html>')) {
-                return response()->json('fail');
-            }
-            if ($res === false || strlen($res) == 0) {
-                return response()->json('fail');
-            }
-            $programfields = json_decode($res, true);
-            foreach ($programfields['oborInfo'] as $programfield) {
-                if ($programfield['forma']=="Prezenční") {
-                    $fields['full'][]=$programfield;
-                }
-                else {
-                    $fields['part'][]=$programfield;
+            if ($programfields = $this->getContents(
+                    'https://stag-ws.utb.cz/ws/services/rest2/programy/getOboryStudijnihoProgramu?' .
+                    "stprIdno=" . $program->id .
+                    "&rok=" . Carbon::now()->year .
+                    "&outputFormat=JSON"
+                )) {
+                foreach ($programfields['oborInfo'] as $programfield) {
+                    if ($programfield['forma'] == "Prezenční") {
+                        $fields['full'][] = $programfield;
+                    }
+                    else {
+                        $fields['part'][] = $programfield;
+                    }
                 }
             }
         }
         return $this->jsonResponse($fields);
+    }
+
+
+    /**
+     * Get array of field's courses.
+     * 
+     * @param int  $id
+     * @param int  $grade
+     * @return Illuminate\Http\Response
+     */
+    public function getCourses(int $id, int $grade)
+    {
+        if ($res = $this->getContents(
+                "https://stag-ws.utb.cz/ws/services/rest2/predmety/getPredmetyByObor?".
+                "oborIdno=" . $id . 
+                "&outputFormat=JSON"
+            )) {
+            foreach ($res['predmetOboru'] as $course) {
+                if ($course['doporucenyRocnik'] && $course['doporucenyRocnik'] >= $grade) {
+                    $courses[$course['doporucenySemestr']][] = $course;
+                }
+            }
+            return $this->jsonResponse($courses);
+        }
+        return $this->jsonResponse("");
     }
 }
--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -11,9 +11,19 @@ class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
+    /**
+     * Get JSON respons in UTF-8 encoding.
+     * 
+     * @param array  $data
+     * @param int  $code
+     * @return Illuminate\Http\Response
+     */
     protected function jsonResponse($data, $code = 200)
     {
-        return response()->json($data, $code,
-        ['Content-Type' => 'application/json;charset=UTF-8', 'Charset' => 'utf-8'], JSON_UNESCAPED_UNICODE);
+        return response()->json($data, $code, [
+                'Content-Type' => 'application/json;charset=UTF-8', 
+                'Charset' => 'utf-8'
+            ], JSON_UNESCAPED_UNICODE
+        );
     }
 }
