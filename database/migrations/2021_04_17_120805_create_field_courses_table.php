0b2d9fb43b27e356ff7080de3635d9e2db9f5bce
Kristýna Tomanová <kristytoman@protonmail.com>
2021-04-19 15:14:13 +0200

    Creating field_courses table

---
+++ database/migrations/2021_04_17_120805_create_field_courses_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFieldCoursesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('field_courses', function (Blueprint $table) {
+            $table->foreignId('home_course_id')
+                  ->constrained('home_courses');
+            $table->foreignId('field_id')
+                  ->constrained('fields');
+            $table->year('grade')->nullable();
+            $table->boolean('is_summer')->nullable();
+            $table->boolean('compulsory');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('field_courses');
+    }
+}
