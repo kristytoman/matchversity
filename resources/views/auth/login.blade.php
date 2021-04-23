f8b73c2717a07962744eef14619b8ff50520a57f
Kristýna Tomanová <kristytoman@protonmail.com>
2021-04-23 17:29:53 +0200

    Login design

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,55 +1,59 @@
 @extends('layouts.app')
 @section('content')
-    <h2>{{ __('Login') }}</h2>
+@include('include.header')
+<div class="w-screen h-container flex justify-center place-items-center">
+<div class="flex flex-col justify-center self-center bg-indigo-800 px-8 py-6 rounded-2xl">
+
+    <h2 class="text-xl text-white font-semibold">Přihlašování</h2>
     <form method="POST"
           action="login">
           @csrf
           <div>
-                <label for="email">
-                       {{ __('E-Mail Address') }}
-                </label>
                 <div>
                     <input id="email"
                            type="email"
                            name="email"
                            value="{{ old('email') }}"
                            autocomplete="email"
+                           placeholder="{{ __('E-Mail Address') }}"
+                           class="my-6 flex-1 bg-indigo-200 flex border w-full border-indigo-400 rounded"
                            required>
                     @error('email')
                         <span>
-                            <strong>{{ $message }}</strong>
+                            <strong class="text-indigo-100">{{ $message }}</strong>
                         </span>
                     @enderror
                 </div>
           </div>
           <div>
-                <label for="password">
-                       {{ __('Password') }}
-                </label>
                 <div>
                     <input id="password"
                            type="password"
                            name="password"
                            autocomplete="current-password"
+                           placeholder="{{ __('Password') }}"
+                           class="my-6 flex-1 bg-indigo-200 flex border w-full border-indigo-400 rounded"
                            required>
                     @error('password')
-                        <span >
-                            <strong>{{ $message }}</strong>
-                        </span>
+                        <strong class="text-indigo-100">{{ $message }}</strong>
                     @enderror
                 </div>
           </div>
-          <div>
+          <div class="flex justify-between text-indigo-100">
+            <label for="remember" class="mr-8">
                 <input id="remember"
                        type="checkbox"
                        name="remember"
+                       class="h-5 w-5 text-indigo-300 rounded-md"
                        {{ old('remember') ? 'checked' : '' }}>
-                <label for="remember">
                        {{ __('Remember Me') }}
                 </label>
-            </div>
+            
             <button type="submit">
                 {{ __('Login') }}
             </button>
+        </div>
     </form>
+</div>
+</div>
 @endsection
