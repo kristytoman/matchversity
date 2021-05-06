4f884bf128c1e2360ecc3417283b03aa3969a9df
Kristýna Tomanová <kristytoman@protonmail.com>
2021-05-06 17:07:24 +0200

    Repair admin login

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -15,7 +15,7 @@ class Authenticate extends Middleware
     protected function redirectTo($request)
     {
         if (! $request->expectsJson()) {
-            if ($request->is('admin/*')) {
+            if ($request->routeIs('admin.*')) {
                 return route('admin.login');
             }
             return route('login');
